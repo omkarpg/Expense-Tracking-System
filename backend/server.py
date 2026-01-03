7042602a41ed314d86508c0aa0092734b8ba9d39
Omkar Gosavi <40800113+omkarpg@users.noreply.github.com>
2026-01-03 10:24:42 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI,HTTPException
 from datetime import date
 import db_helper
 from typing import List
@@ -6,50 +6,48 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-
-class Expense(BaseModel):
-    amount: float
+class Expenses(BaseModel):
+    amount:float
     category: str
     notes: str
 
-
 class DateRange(BaseModel):
-    start_date: date
+    start_date:date
     end_date: date
 
-
-@app.get("/expenses/{expense_date}", response_model=List[Expense])
+@app.get("/expenses/{expense_date}", response_model=List[Expenses])
 def get_expenses(expense_date: date):
-    expenses = db_helper.fetch_expenses_for_date(expense_date)
-    if expenses is None:
-        raise HTTPException(status_code=500, detail="Failed to retrieve expenses from the database.")
+    data = db_helper.fetch_expenses_for_date(expense_date)
+    return data
 
-    return expenses
-
+@app.get("/analytics_by_month/")
+def get_analytics_by_month():
+    data = db_helper.fetch_expense_by_month()
+    return data
 
 @app.post("/expenses/{expense_date}")
-def add_or_update_expense(expense_date: date, expenses:List[Expense]):
-    db_helper.delete_expenses_for_date(expense_date)
+def add_or_update_expense(expense_date:date, expenses:List[Expenses]):
+    db_helper.delete_expense_for_date(expense_date)
     for expense in expenses:
-        db_helper.insert_expense(expense_date, expense.amount, expense.category, expense.notes)
+        db_helper.insert_expense(expense_date,expense.amount,expense.category,expense.notes)
 
-    return {"message": "Expenses updated successfully"}
-
+    return "Message: Expense updated successfully"
 
 @app.post("/analytics/")
 def get_analytics(date_range: DateRange):
     data = db_helper.fetch_expense_summary(date_range.start_date, date_range.end_date)
     if data is None:
-        raise HTTPException(status_code=500, detail="Failed to retrieve expense summary from the database.")
-
-    total = sum([row['total'] for row in data])
+        raise HTTPException(status_code = 500, detail = f"Failed to retrieve expense summary from the database")
+    total = 0
+    for row in data:
+        total += row['Total']
 
     breakdown = {}
-    for row in data:
-        percentage = (row['total']/total)*100 if total != 0 else 0
-        breakdown[row['category']] = {
-            "total": row['total'],
-            "percentage": percentage
-        }
 
+    for row in data:
+        percentage = (row['Total']/total)*100 if total != 0 else 0
+        breakdown[row['category']] = {
+            'total': row["Total"],
+            'percentage': percentage
+        }
     return breakdown
